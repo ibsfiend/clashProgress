f6bc35a85bdf4dd741e4e37cf848bcbd8d92b4ef
jake-williams22 <jwill73@g.clemson.edu>
2025-03-29 19:08:28 -0400

    refactor JSON handling in FileHandler to use Newtonsoft.Json

--- src/file_handler.cs
+++ src/file_handler.cs
@@ -1,7 +1,8 @@
 using System;
 using System.IO;
-using System.Text.Json;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+
 namespace clashProgress.src {
     public class FileHandler {
         public static T LoadJson<T>(string path) {
@@ -11,7 +12,7 @@ namespace clashProgress.src {
 
             try {
                 string jsonString = File.ReadAllText(path);
-                return JsonSerializer.Deserialize<T>(jsonString);
+                return JsonConvert.DeserializeObject<T>(jsonString);
             } catch (Exception e) {
                 throw new Exception($"Error loading JSON file: {e.Message}");
             }
@@ -19,10 +20,8 @@ namespace clashProgress.src {
 
     public static void SaveJson<T>(string path, T data) {
         try {
-            string jsonString = JsonSerializer.Serialize(data, new JsonSerializerOptions {
-                WriteIndented = true
-            });
-                File.WriteAllText(path, jsonString);
+            string jsonString = JsonConvert.SerializeObject(data, Formatting.Indented);
+            File.WriteAllText(path, jsonString);
             } catch (Exception e) {
                 throw new Exception($"Error saving JSON file: {e.Message}");
             }
--- test/file_handler_test1.cs
+++ test/file_handler_test1.cs
@@ -2,13 +2,16 @@ using System;
 using System.IO;
 using System.Text.Json;
 using System.Collections.Generic;
+using clashProgress.src;
 using clashProgress;
 
 public class FileHandlerTest {
     public static void Main(string[] args) {
         string path = "data/progress.json";
-        Progress progress = FileHandler.LoadJson<Progress>(path);
+        Root progress = FileHandler.LoadJson<Root>(path);
         Console.WriteLine(progress);
+        Console.WriteLine("Archer Queen Level: " + (progress?.Heroes?.ArcherQueen ?? 0));
+        Console.WriteLine("Number of Archer Towers: " + (progress?.Buildings?.Defense?.ArcherTowers?.Count ?? 0));
     }
 }
 
