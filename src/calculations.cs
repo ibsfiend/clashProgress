2af1ef157ba26f37b5f3d5f602746d3523dfb33d
jake-williams22 <jwill73@g.clemson.edu>
2025-03-31 08:29:53 -0400

    Add time conversion methods to Calculations class
    Implement methods to convert time from a string format to seconds and vice versa in the Calculations class. The convertToSeconds method is designed to parse a time string formatted as "Xd Xh Xm Xs" into total seconds, while convertToTime converts seconds back into the formatted string. This addition enhances the functionality of the calculations module.

---
+++ src/calculations.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace Calculations
+{
+    public class Calculations
+    {
+        // Converts time from format "1d 2h 3m 4s" to seconds
+        public static double convertToSeconds(string time)
+        {
+            // Split the time string into parts
+            string[] parts = time.Split(' ');
+
+            // Initialize seconds to 0
+            double seconds = 0;
+            for (int i = 0; i < parts.Length; i++)
+            {
+                // Split the part into a number and a unit
+                string[] part = parts[i].Split(new char[] { 'd', 'h', 'm', 's' }, StringSplitOptions.RemoveEmptyEntries);
+                
+                
+            }
+            return seconds;
+        }
+
+        // Converts seconds to time in format "1d 2h 3m 4s"
+        public static string convertToTime(double seconds)
+        {
+            // Calculate the number of days, hours, minutes, and seconds
+            int days = (int)(seconds / 86400);
+            int hours = (int)((seconds % 86400) / 3600);
+            int minutes = (int)((seconds % 3600) / 60);
+            int secs = (int)(seconds % 60);
+
+            // Build the time string
+            string time = "";
+            if (days > 0) time += days + "d ";
+            if (hours > 0) time += hours + "h ";
+            if (minutes > 0) time += minutes + "m ";
+            if (secs > 0) time += secs + "s";
+
+            return time;
+        }
+        
+    }
+}
