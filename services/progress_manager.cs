d423ed51fa2645e5d2e8f76ae1ffc7f88b40c4b1
jake-williams22 <jwill73@g.clemson.edu>
2025-04-09 17:47:27 -0400

    Add GetCurrentTHLevel method to ProgressManager
    - Implemented GetCurrentTHLevel to retrieve the maximum level of the Town Hall from the buildings data.
    - Introduced a new static method timeToMax that utilizes GetCurrentTHLevel to determine the current Town Hall level.

--- services/progress_manager.cs
+++ services/progress_manager.cs
@@ -5,6 +5,15 @@ using clashProgress.models.maxTH;
 
 namespace clashProgress.services {
     public class ProgressManager {
-
+        private static int GetCurrentTHLevel(Dictionary<string, Dictionary<string, List<LevelCount>>> buildings) {
+            if (buildings.TryGetValue("Defense", out var defenseBuildings) && defenseBuildings.TryGetValue("Town Hall", out var townHallList)) {
+                return townHallList.Max(th => th.Level ?? 0);
+            }
+            throw new Exception("Town Hall not found in buildings data.");
         }
+
+    public static timeToMax() {
+        var currentTHLevel = GetCurrentTHLevel(buildings);
+    }
+    }
 }
