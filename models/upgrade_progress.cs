2da5b5084f0545dda5bfe605fad4798925fe6352
jake-williams22 <jwill73@g.clemson.edu>
2025-04-18 09:55:30 -0400

    add upgrade_progress.cs to models directory for progress_manager.cs

---
+++ models/upgrade_progress.cs
@@ -0,0 +1,8 @@
+namespace clashProgress.models.upgradeProgress {
+
+    public class UpgradeCost {
+        public double timeCost  {get; set;}
+        public Dictionary<string, double> resourceCost {get; set;}
+    }
+
+}
