425fe44a08839bbe9f6fbfaa889bce0413dbc7f1
jake-williams22 <jwill73@g.clemson.edu>
2025-04-18 20:20:35 -0400

    Refactor progress data structure to include Player Progress encapsulation
    - Updated progress.json to introduce a new "Player Progress" object, organizing buildings, army, resources, traps, support, heroes, troops, spells, and siege machines under this structure.
    - Modified progress_data.cs to reflect the new data structure, creating a PlayerProgressContainer class for better data management.

--- models/progress_data.cs
+++ models/progress_data.cs
@@ -13,23 +13,27 @@ namespace clashProgress.models.progress
         [JsonPropertyName("owned")]
         public int? Owned { get; set; }
 
-}
+    }
 
+    public class PlayerProgressContainer
+    {
+        [JsonPropertyName("PlayerProgress")]
+        public PlayerProgress Progress { get; set; }
+    }
+    public class PlayerProgress
+    {
+        public Dictionary<string, Dictionary<string, List<LevelCount>>>? Buildings { get; set; }
+        public Dictionary<string, int>? Heroes { get; set; }
+        public Dictionary<string, Dictionary<string, List<EquipmentItem>>>? HeroEquipment { get; set; }
+        public Dictionary<string, Dictionary<string, int>>? Troops { get; set; }
+        public Dictionary<string, Dictionary<string, int>>? Spells { get; set; }
+        public Dictionary<string, int>? SiegeMachines { get; set; }
+    }
 
-public class Root
-{
-    public Dictionary<string, Dictionary<string, List<LevelCount>>>? Buildings { get; set; }
-    public Dictionary<string, int>? Heroes { get; set; }
-    public Dictionary<string, Dictionary<string, List<EquipmentItem>>>? HeroEquipment { get; set; }
-    public Dictionary<string, Dictionary<string, int>>? Troops { get; set; }
-    public Dictionary<string, Dictionary<string, int>>? Spells { get; set; }
-    public Dictionary<string, int>? SiegeMachines { get; set; }
-}
-
-public class EquipmentItem
-{
-    public string? Rarity { get; set; }
-    public int? Level { get; set; }
-}
+    public class EquipmentItem
+    {
+        public string? Rarity { get; set; }
+        public int? Level { get; set; }
+    }
 
 }
