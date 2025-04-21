b4b438092c8f0577ac574a2667bc716b01505a49
jake-williams22 <jwill73@g.clemson.edu>
2025-04-21 17:43:31 -0400

    change getProgress methods to return UpgradeCosts

--- services/progress_manager.cs
+++ services/progress_manager.cs
@@ -3,6 +3,7 @@ using clashProgress.utils;
 using clashProgress.models.progress;
 using clashProgress.models.maxTH;
 using clashProgress.models.upgrade_analyzer;
+using clashProgress.models.upgradeProgress;
 
 namespace clashProgress.services {
     public class ProgressManager {
@@ -44,9 +45,9 @@ namespace clashProgress.services {
         /// 4. Adds the progress values together
         /// 5. Returns the total progress
         /// </remarks>
-        public static double GetTotalLevelProgress(List<UpgradeInfoEntry> missingUpgrades)
+        public static UpgradeCost GetTotalLevelProgress(List<UpgradeInfoEntry> missingUpgrades)
         {
-            double total = 0;
+            var totalCost = new UpgradeCost();
             foreach (var upgrade in missingUpgrades)
             {
                 foreach (var step in upgrade.Steps)
@@ -54,22 +55,26 @@ namespace clashProgress.services {
                     total += 1; // or normalize based on max level to get a percentage-based system
                 }
             }
-            return total;
+            return totalCost;
         }
 
 
-        public static double getBuildingProgress(string building, int level, Dictionary<string, Dictionary<string, List<LevelCount>>> buildings) {
+        public static UpgradeCost getBuildingProgress(string building, int level, Dictionary<string, Dictionary<string, List<LevelCount>>> buildings) {
             var buildingList = buildings["Defense"][building];
-                
+            var buildingCost = new UpgradeCost();
+            foreach (var step in buildingList) {
+                buildingCost.resourceCost[step.Key] = step.Value.Count;
+            }
+            return buildingCost;
         }
 
-        public static double getHeroProgress(string hero, Dictionary<string, int> heroes) {
+        public static UpgradeCost getHeroProgress(string hero, Dictionary<string, int> heroes) {
             var heroLevel = heroes[hero];
             var maxHeroLevel = THData.MaxTHLevels[hero];
             return (double)heroLevel / maxHeroLevel;
         }
 
-        public static double getTroopProgress(string troop, int level, Dictionary<string, Dictionary<string, int>> troops) {
+        public static UpgradeCost getTroopProgress(string troop, int level, Dictionary<string, Dictionary<string, int>> troops) {
             var troopList = troops["Troops"][troop];
         }
     }
